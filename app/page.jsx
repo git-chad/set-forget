0d3721c40bbe69601fbef29593c5fbbb3cc344fa
git-chad <tobimocc@gmail.com>
2024-02-02 17:54:18 -0300

    chore(dev): smooth scroll

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,28 @@
-import Image from "next/image";
+"use client";
 import Hero from "@/app/components/home/hero-module/hero";
 import StatsPage from "./components/home/stats-module/statsPage";
 import Carousels from "./components/ecyce/carousels";
 import WorkPage from "./components/home/our-work-module/workPage";
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main>
-      <Hero/>
-      <StatsPage/>
-      <Carousels type='work'/>
-      {/* <Carousels type='talk'/> */}
-      <WorkPage/>
+      <Hero />
+      <StatsPage />
+      <Carousels type="work" />
+      <WorkPage />
     </main>
   );
 }
